5c40b672b5794d4015279e616e82eac7b44b47f0
Russ Green <1886088+russgreen@users.noreply.github.com>
2026-01-01 08:16:47 +0000

    removed old projects

--- BACApp.App/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace BACApp.App;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
