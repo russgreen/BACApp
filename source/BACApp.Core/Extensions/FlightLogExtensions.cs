b1ea7cd0c4aff9c54ee26e566bbf9625822258ff
Russ Green <1886088+russgreen@users.noreply.github.com>
2026-01-13 07:32:54 +0000

    used aircraft settings for charge time adjustments. closes #25

---
+++ source/BACApp.Core/Extensions/FlightLogExtensions.cs
@@ -0,0 +1,26 @@
+﻿using BACApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BACApp.Core.Extensions;
+
+public static class FlightLogExtensions
+{
+    public static void SetChargeTime(this FlightLog flightLog, int? useBlockTime, int? minutesAdjust)
+    {
+        var chargeTime = flightLog.FlightTime;
+
+        if (Convert.ToBoolean(useBlockTime))
+        {
+            chargeTime = flightLog.BlockTime;
+        }
+
+        if(minutesAdjust != 0)
+        {
+            chargeTime = chargeTime.Add(TimeSpan.FromMinutes(Convert.ToDouble(minutesAdjust)));
+        }
+
+        flightLog.ChargeTime = chargeTime;
+    }
+}
